d36b338424aed0af9b090a6b04b0b2bfddcee5c9
Vishal Kumar. S <vk28.02.2004@gmail.com>
2023-12-16 21:00:22 +0530

    updated requiremetns.txt, predict_prod_data() toa dd visualisations, updated ci-cd.yml file,to include CML

--- steps/predict_prod_data.py
+++ steps/predict_prod_data.py
@@ -3,7 +3,7 @@ from typing import Tuple, Annotated
 import h2o
 from h2o.automl import H2OAutoML
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 
 @step(enable_cache = False)
@@ -17,6 +17,27 @@ def predict_prod_data(ref_data: pd.DataFrame, curr_data: pd.DataFrame) -> Tuple[
     # Make predictions on the reference and current datasets
     ref_data['prediction'] = trained_model.predict(h2o.H2OFrame(ref_data)).as_data_frame()
     curr_data['prediction'] = trained_model.predict(h2o.H2OFrame(curr_data)).as_data_frame()
+
+    # Generate a scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ref_data['prediction'], ref_data['Approved_Conversion'], label='Reference Data')
+    plt.scatter(curr_data['prediction'], curr_data['Approved_Conversion'], label='Current Data')
+    plt.title('Predictions vs Actual')
+    plt.xlabel('Predictions')
+    plt.ylabel('Actual')
+    plt.legend()
+    plt.savefig('CML_Reports/predictions_scatter_plot.png')
+    
+    # Generate a residuals plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(ref_data['prediction'], ref_data['prediction'] - ref_data['Approved_Conversion'], label='Reference Data')
+    plt.scatter(curr_data['prediction'], curr_data['prediction'] - curr_data['Approved_Conversion'], label='Current Data')
+    plt.title('Residuals Plot')
+    plt.xlabel('Predictions')
+    plt.ylabel('Residuals')
+    plt.legend()
+    plt.savefig('CML_Reports/residuals_plot.png')
+
     print(ref_data.head(5))
     print(curr_data.head(5))
     return ref_data, curr_data

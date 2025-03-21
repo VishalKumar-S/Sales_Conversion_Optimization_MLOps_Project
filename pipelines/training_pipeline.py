5a8014607c6d276ad38fd7c006dff64cfd4e74e0
Vishal Kumar. S <vk28.02.2004@gmail.com>
2025-03-21 18:29:45 +0530

    Changed url to path

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -5,19 +5,20 @@ from steps.train_model import train_model, model_performance_validation
 from steps.alert_report import alert_report
 
 class TrainPipelineBuilder:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, path):
+        self.path = path
         self.user_email="vishalkumar.s2022ai-ds@sece.ac.in"
 
     def build(self):
-        df = ingest_data(self.url)
+        df = ingest_data(self.path)
         data_quality_validated_dataset = data_quality_validation(df, self.user_email)
         cleaned_data, reference_dataset, current_dataset = clean_data(df)
         reference_dataset, current_dataset = data_drift_validation(reference_dataset, current_dataset, self.user_email)
         train_df, test_df = train_model(cleaned_data)
         model_performance_validation(train_df, test_df, self.user_email)
 
-@pipeline(enable_cache=False)
-def train_pipeline(url: str):
-    builder = TrainPipelineBuilder(url)
+
+@pipeline(enable_cache=True)
+def train_pipeline(path: str):
+    builder = TrainPipelineBuilder(path)
     builder.build()

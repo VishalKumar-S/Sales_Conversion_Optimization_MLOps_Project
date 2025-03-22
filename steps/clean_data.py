eb24c96160aefa782ecedcf22eccf11e664e2e57
Vishal Kumar. S <vk28.02.2004@gmail.com>
2025-03-22 10:43:44 +0530

    Added notes

--- steps/clean_data.py
+++ steps/clean_data.py
@@ -14,10 +14,14 @@ from neptune.types import File
 import neptune
 from zenml.integrations.neptune.experiment_trackers.run_state import get_neptune_run
 import streamlit as st
+import sys
+
 
 class DataCleaner:
     @staticmethod
     def clean(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame]:
+        #Note: Here, the target column is removed since data drift validation is checked only for the input data, as in production, for batch dataset, only input data would be given, the target variable, we need to predict. Another reason is,including the target in drift analysis could give misleading results, as target shifts might be legitimate business changes rather than problematic data drift
+
         preprocessor = DataPreprocessor(df)
         cleaned_data = preprocessor.clean_data()
         cleaned_data_without_target= cleaned_data.copy()
@@ -28,7 +32,7 @@ class DataDriftValidator:
     @staticmethod
     def validate(reference_dataset: pd.DataFrame, current_dataset: pd.DataFrame, user_email: str)-> Tuple[pd.DataFrame, pd.DataFrame]:
         test_suite = TestSuite(tests=[DataDriftTestPreset(),])
-        test_suite.run(reference_data=reference_dataset, current_data=current_dataset)
+        test_suite.run(reference_data= reference_dataset, current_data=current_dataset)
         threshold = test_suite.as_dict()['summary']['success_tests'] / test_suite.as_dict()['summary']['total_tests']
         test_name = "Data Drift Test"
         passed_tests = test_suite.as_dict()['summary']['success_tests']
@@ -38,20 +42,24 @@ class DataDriftValidator:
         st.write(f"Number of passed tests: {passed_tests} ✅, "
              f"Number of failed tests: {failed_tests} ❌, "
              f"Out of {total_tests} tests conducted in Data Drift.")
-        if threshold < 0.65:
 
-            # Initialize a run
+
+        if threshold < 0.65:
+            logging.error("Data drift tests got failed. Logging failed reports and sending alerts...")
             neptune_run = get_neptune_run()
 
             test_suite.save_html("Evidently_Reports/data_drift_suite.html")
             neptune_run["html/Data Drift Test"].upload("Evidently_Reports/data_drift_suite.html")
             alert_report(passed_tests, failed_tests, total_tests, "Data Drift Test", "Evidently_Reports/data_drift_suite.html", user_email)
+            sys.exit("Data drift threshold failed. Pipeline terminated.")
         else:
             logging.info(f"All Data Drift checks passed")
             return reference_dataset, current_dataset
 
-@step(enable_cache=False)
+
+@step(enable_cache=True)
 def clean_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    '''By separating the ZenML pipeline steps(clean_data()) from the actual implementation logic(DataCleaner.clean()), I'm creating a clear boundary between orchestration and business logic. The step functions manage the workflow and error handling, while the class methods contain the actual data processing logic. Also,it allows for testing independednt components logic, and re-use the independent components logic'''
     try:
         cleaned_data = DataCleaner.clean(df)
         cleaned_data, cleaned_data_without_target = DataCleaner.clean(df)
@@ -61,7 +69,9 @@ def clean_data(df: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFra
         logging.error(e)
         raise e
 
-@step(enable_cache=False)
+
+
+@step(experiment_tracker="neptune_experiment_tracker",enable_cache=True)
 def data_drift_validation(reference_dataset: pd.DataFrame, current_dataset: pd.DataFrame, user_email: str) -> Tuple[pd.DataFrame, pd.DataFrame]:
     try:
         reference_dataset, current_dataset = DataDriftValidator.validate(reference_dataset, current_dataset, user_email)

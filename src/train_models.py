a03bed013015f0ef7bd0c5ef828ce49c0552d464
Vishal Kumar. S <vk28.02.2004@gmail.com>
2025-03-22 10:40:52 +0530

    Imrpoved code of model training

--- src/train_models.py
+++ src/train_models.py
@@ -6,7 +6,7 @@ import neptune
 import datetime
 import os
 
-# Example usage:
+
 timestamp = datetime.datetime.now().strftime("%d%H%M")
 model_key = f"MOD{timestamp}"
 
@@ -21,6 +21,7 @@ class ModelFactory:
         )
         return model
 
+
 def train_h2o_automl(cleaned_dataset):
     model = ModelFactory.create_model(
     name="Prediction model H20.ai AutoML",
@@ -28,31 +29,33 @@ def train_h2o_automl(cleaned_dataset):
     project="Vishal-Kumar-S/Sales-Conversion-Optimisation-MLOps-Project",
     api_token=os.environ.get('API_TOKEN'),)
 
-    # Initialize H2O
+    # Initialize H2O AutoML
     h2o.init()
-    # Load data
+
     data = cleaned_dataset
+
     # Convert Pandas DataFrame to H2O DataFrame
     h2o_data = h2o.H2OFrame(data)
 
-    # Specify the response and predictor columns
+
+    #  Response = y, Predictors = x
     response = "Approved_Conversion"
     predictors = data.columns.drop(response).tolist()
 
-    # Split the data into train and test sets for model validation
+
     train, test = h2o_data.split_frame(ratios=[0.8], seed=42)
+
 
     # Initialize and train the H2OAutoML model
     aml = H2OAutoML(max_models=20, seed=42)
     aml.train(x=predictors, y=response, training_frame=train)
 
+
     # Save the best model
     model_path = "./models/best_model.zip"
     best_model = aml.leader
-    print(best_model)
 
     model_type = best_model.model_id 
-    print(model_type)
 
     model["model_type"] = best_model.model_id
     model["hyperparameters"] = best_model.params
@@ -62,22 +65,23 @@ def train_h2o_automl(cleaned_dataset):
     # Get model performance
     perf = best_model.model_performance()
 
-    # Log model performance metrics to Neptune
+    # Log model performance metrics to Neptune.ai
     model["r2"].log(perf.r2())
     model["mse"].log(perf.mse())
     model["rmse"].log(perf.rmse())
     model["rmsle"].log(perf.rmsle())
     model["mae"].log(perf.mae())
 
-    # Get predictions for train and test datasets
+
+    # Predict
     train_pred = aml.predict(train)
     test_pred = aml.predict(test)
 
-    # Convert H2OFrames to pandas DataFrames
+    # Convert H2OFrames back to pandas DataFrames
     train_df = train.as_data_frame()
     test_df = test.as_data_frame()
 
-    # Add predictions to the original train and test datasets
+    # Include predictions as an attribute to the original train and test datasets
     train_df['prediction'] = train_pred.as_data_frame()
     test_df['prediction'] = test_pred.as_data_frame()
 
